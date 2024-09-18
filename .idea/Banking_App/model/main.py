424dd3f1b006f10c96399a1cc0c90c56a81af905
Fink József <erronblack0451@gmail.com>
2024-09-18 14:53:32 +0200

    létrehoztam a bank controllert

--- .idea/Banking_App/model/main.py
+++ .idea/Banking_App/model/main.py
@@ -1,7 +1,12 @@
 import math
 from Account import Account
+from BankController import BankController
 
+# Create a BankController instance and start the menu
+if __name__ == "__main__":
+    bank_controller = BankController()
+    bank_controller.menu()
 
 uj_account = Account.create_account()
 uj_account.deposit(5)
-print(uj_account.get_balance)
+print(uj_account.get_account_info())
