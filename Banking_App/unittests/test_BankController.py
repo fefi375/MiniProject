6bd2a86c68a428a80a18c6266d51a46c9605a0ca
Fink József <erronblack0451@gmail.com>
2024-09-23 15:56:43 +0200

    teszt arra hogy üres JSON fileal a fiókok listálya is üres

--- Banking_App/unittests/test_BankController.py
+++ Banking_App/unittests/test_BankController.py
@@ -5,4 +5,10 @@ from Banking_App.model.Account import Account
 from Banking_App.controller.BankController import BankController
 
 class TestBankController(unittest.TestCase):
-    pass
+    
+    @patch('builtins.open', new_callable=mock_open, read_data='[]')
+    @patch('os.path.exists', return_value=True)
+    def test_load_accounts_empty(self, mock_exists, mock_open_file):
+        """Fiókok betöltésének a tesztje ugy hogy a JSON file üres."""
+        bank_controller = BankController()
+        self.assertEqual(bank_controller.accounts, {}, "A fiókok listályának üresnek kell lennie amikor a JSON file üres")
