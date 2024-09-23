3cfdfce56a1fdfe0258a403d1788c56564bf8d94
Fink József <erronblack0451@gmail.com>
2024-09-23 15:52:18 +0200

    Létrehoztam a bankcontroller test osztályt

---
+++ Banking_App/unittests/test_BankController.py
@@ -0,0 +1,8 @@
+import unittest
+from unittest.mock import patch, mock_open, MagicMock
+import json
+from Banking_App.model.Account import Account
+from Banking_App.controller.BankController import BankController
+
+class TestBankController(unittest.TestCase):
+    pass
