196bbb0e866c79358b7981c3dc83334b27a869fb
Szűcs Ferenc <fefi375@gmail.com>
2024-09-26 15:37:51 +0200

    get balance metódus tesztelése

--- Banking_App/unittests/test_account.py
+++ Banking_App/unittests/test_account.py
@@ -4,7 +4,7 @@ from Banking_App.model.Account import Account
 #Account 0 egyenleggel.
 accountZero = Account("John", "Wick", "3456", 0.0)
 
-accountOneThousand=Account("Nagy", "Helga", "3456", 1000)
+accountOneThousand=Account("Nagy", "Helga", "3656", 1000)
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,18 +12,8 @@ class MyTestCase(unittest.TestCase):
     def test_balance(self):
         self.assertEqual(accountZero.get_balance(), 0.0)  # add assertion here
         self.assertEqual(accountOneThousand.get_balance(), 1000)
+        self.assertEqual(accountOneThousand.get_balance()-1000, 0)
 
-    def test_withdraw(self):
-        self.assertRaises(ValueError, accountZero.withdraw, 1000)
-        self.assertRaises(ValueError, accountOneThousand.withdraw, 0)
-        self.assertRaises(ValueError, accountOneThousand.withdraw, 10001)
-
-
-    def test_deposit(self):
-        accountDeposit = Account("Deposit", "Test", "0001", 0.0)
-        accountDeposit.deposit(10.0)
-        self.assertEqual(accountDeposit.get_balance(), 10.0)
-        self.assertRaises(ValueError, accountDeposit.deposit, 0)
 
     
 
